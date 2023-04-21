af58209ee97cc67fdc19aea95d71df47986c4c08
Takuya Ishizawa <ishizawa.takuya@classmethod.jp>
2023-04-21 10:34:29 +0900

    グループ毎に疎通可能にする

--- transit_gateway_rtb.tf
+++ transit_gateway_rtb.tf
@@ -1,30 +1,45 @@
 # Route Table for Workload-VPCs
 resource "aws_ec2_transit_gateway_route_table" "workload_vpc" {
+  count = length(var.group)
+
   transit_gateway_id = aws_ec2_transit_gateway.main.id
 }
 
 resource "aws_ec2_transit_gateway_route" "workload_vpc_to_internet" {
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload_vpc.id
+  count = length(var.group)
+
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload_vpc[count.index].id
   destination_cidr_block         = "0.0.0.0/0"
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.internet.id
 }
 
 resource "aws_ec2_transit_gateway_route" "block_inter_workloads" {
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload_vpc.id
+  count = length(var.group)
+
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload_vpc[count.index].id
   destination_cidr_block         = local.workload_pool_cidr
   blackhole                      = true
 }
 
 resource "aws_ec2_transit_gateway_route_table_propagation" "workload_vpc_to_common_service_vpc" {
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload_vpc.id
+  count = length(var.group)
+
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload_vpc[count.index].id
   transit_gateway_attachment_id  = module.common_service_vpc.transit_gateway_attachment_id
 }
 
 resource "aws_ec2_transit_gateway_route_table_association" "workload_vpc" {
-  count = var.workload_count
+  for_each = merge([for v in [for k_1, v_1 in zipmap(range(length(var.group)), var.group) : [for v_2 in v_1 : [tonumber(k_1), v_2]]] : { for v_3 in v : join(",", v_3) => v_3 }]...)
 
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload_vpc.id
-  transit_gateway_attachment_id  = module.workload_vpc[count.index].transit_gateway_attachment_id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload_vpc[each.value[0]].id
+  transit_gateway_attachment_id  = module.workload_vpc[each.value[1]].transit_gateway_attachment_id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "workload_vpc" {
+  for_each = merge([for v in [for k_1, v_1 in zipmap(range(length(var.group)), var.group) : [for v_2 in v_1 : [tonumber(k_1), v_2]]] : { for v_3 in v : join(",", v_3) => v_3 }]...)
+
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload_vpc[each.value[0]].id
+  transit_gateway_attachment_id  = module.workload_vpc[each.value[1]].transit_gateway_attachment_id
 }
 
 # Route Table for Infrastructure-VPCs
--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
 variable "system_id" {}
 variable "toplevel_pool_cidr" {}
-variable "workload_count" {}
+variable "workload_count" { type = number }
+variable "group" { type = list(list(number)) }
