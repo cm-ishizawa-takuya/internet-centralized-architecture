08f97ce06e90c01f028e050af67b7fc007861000
Takuya Ishizawa <ishizawa.takuya@classmethod.jp>
2023-04-21 12:04:01 +0900

    TGWルートテーブルに共通サービス用の設定を追加

--- transit_gateway_rtb.tf
+++ transit_gateway_rtb.tf
@@ -3,7 +3,7 @@ resource "aws_ec2_transit_gateway_route_table" "workload_vpc" {
   transit_gateway_id = aws_ec2_transit_gateway.main.id
 }
 
-resource "aws_ec2_transit_gateway_route" "to_internet" {
+resource "aws_ec2_transit_gateway_route" "workload_vpc_to_internet" {
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload_vpc.id
   destination_cidr_block         = "0.0.0.0/0"
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.internet.id
@@ -15,6 +15,11 @@ resource "aws_ec2_transit_gateway_route" "block_inter_workloads" {
   blackhole                      = true
 }
 
+resource "aws_ec2_transit_gateway_route_table_propagation" "workload_vpc_to_common_service_vpc" {
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload_vpc.id
+  transit_gateway_attachment_id  = module.common_service_vpc.transit_gateway_attachment_id
+}
+
 resource "aws_ec2_transit_gateway_route_table_association" "workload_vpc" {
   count = var.workload_count
 
@@ -22,19 +27,35 @@ resource "aws_ec2_transit_gateway_route_table_association" "workload_vpc" {
   transit_gateway_attachment_id  = module.workload_vpc[count.index].transit_gateway_attachment_id
 }
 
-# Route Table for Internet-VPC
-resource "aws_ec2_transit_gateway_route_table" "internet_vpc" {
+# Route Table for Infrastructure-VPCs
+resource "aws_ec2_transit_gateway_route_table" "infrastructure_vpc" {
   transit_gateway_id = aws_ec2_transit_gateway.main.id
 }
 
-resource "aws_ec2_transit_gateway_route_table_propagation" "internet_vpc" {
+resource "aws_ec2_transit_gateway_route" "infrastructure_vpc_to_internet" {
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.infrastructure_vpc.id
+  destination_cidr_block         = "0.0.0.0/0"
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.internet.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "infrastructure_vpc_to_workload_vpc" {
   count = var.workload_count
 
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.internet_vpc.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.infrastructure_vpc.id
   transit_gateway_attachment_id  = module.workload_vpc[count.index].transit_gateway_attachment_id
 }
 
+resource "aws_ec2_transit_gateway_route_table_propagation" "internet_vpc_to_common_service_vpc" {
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.infrastructure_vpc.id
+  transit_gateway_attachment_id  = module.common_service_vpc.transit_gateway_attachment_id
+}
+
 resource "aws_ec2_transit_gateway_route_table_association" "internet_vpc" {
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.internet_vpc.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.infrastructure_vpc.id
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.internet.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "common_service_vpc" {
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.infrastructure_vpc.id
+  transit_gateway_attachment_id  = module.common_service_vpc.transit_gateway_attachment_id
 }
